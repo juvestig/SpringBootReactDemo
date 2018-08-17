fdebe40260cf89bc9d1412f9d64a1540780aad9c
juvestig <juvestig@gmail.com>
2018-08-17 16:19:08 -0700

    Initial commit from springboot io project template

---
+++ src/main/java/com/demo/exercise/SpringBootReactDemo/SpringBootReactDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.exercise.SpringBootReactDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactDemoApplication.class, args);
+	}
+}
